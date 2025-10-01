16e2ab26afecc7f58a4a361f5a5418fa0b0bebb6
mari00ana <molygomes123@gmail.com>
2025-10-01 14:25:22 -0300

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <></>
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <img 
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv1wkgNWnMF3W0DKuA2XBFKSvP6QHgTD1HWg&s" 
+          alt="Campanha de Vacinação" 
+        />
+        <h1>Bem-vindo à Campanha de Vacinação</h1>
+        <p>Proteja-se e proteja quem você ama.
+          <br /> Vacine-se!</p>
+        <div className="ctas">
+       
+        </div>
+      </main>
+    </div>
   );
 }
